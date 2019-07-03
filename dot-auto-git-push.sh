25f0a9bf80ffcd4c6bed2ec736b73537db9cdab6
hirono hideki <hideki0001@gmail.com>
2019-07-03 18:25:13 +0900

    Windows10

--- dot-auto-git-push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SYS_NAME=`uname -a|cut -d' ' -f2`
-DATE=`date '+%Y-%m-%d_%H%M'`
-COMMIT_MES="update ${DATE} ${SYS_NAME}"
-git add -A && git commit -m "$COMMIT_MES"  && git push
-
-echo "${COMMIT_MES} DONE"
-
